ce93636682fb82edcb80147f49c63aaa0555abee
capimaker <capimaker@gmail.com>
2025-06-29 13:43:11 +0200

    ordercontroller de vuelta a la normalidad

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,4 +1,4 @@
-/*const Order = require('../models/order');
+const Order = require('../models/order');
 const Product = require('../models/product');
 const User = require('../models/user');
 
@@ -69,81 +69,4 @@ const OrderController = {
   },
 };
 
-module.exports = OrderController; */
-
-const Order = require('../models/order');
-const Product = require('../models/product');
-const User = require('../models/user');
-
-const OrderController = {
-  async create(req, res) {
-    try {
-      const { productIds } = req.body;
-      const userId = req.user._id; //  Se toma del token, no del body
-
-      const user = await User.findById(userId);
-      if (!user) {
-        return res.status(404).json({ error: 'Usuario no encontrado' });
-      }
-
-      const validProducts = await Product.find({
-        _id: { $in: productIds },
-      });
-
-      if (validProducts.length === 0) {
-        return res.status(400).json({ error: 'No se encontraron productos válidos' });
-      }
-
-      const productIdsClean = validProducts.map((product) => product._id);
-
-      const order = new Order({
-        user: userId,
-        products: productIdsClean,
-      });
-
-      await order.save();
-
-      user.orders.push(order._id);
-      await user.save();
-
-      await Promise.all(
-        validProducts.map(async (product) => {
-          product.orders.push(order._id);
-          await product.save();
-        })
-      );
-
-      res.status(201).json({
-        msg: 'Orden creada con éxito',
-        order,
-      });
-    } catch (error) {
-      res.status(500).json({
-        error: 'Error al crear la orden',
-        details: error.message,
-      });
-    }
-  },
-
-  async getAllOrdersWithProducts(req, res) {
-    try {
-      const orders = await Order.find({ user: req.user._id }) // solo pedidos del usuario logueado
-        .select('_id user products createdAt updatedAt')
-        .populate({
-          path: 'user',
-          select: 'name last_name adress email',
-        })
-        .populate({
-          path: 'products',
-          select: 'name price',
-        });
-
-      res.status(200).json(orders);
-    } catch (error) {
-      res.status(500).json({ error: 'No se han podido obtener los pedidos', details: error.message });
-    }
-  },
-};
-
-module.exports = OrderController;
-
+module.exports = OrderController; 
